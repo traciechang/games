66755a04fdda44d4e467a52c293ecc210a38417e
traciechang <traciechang@gmail.com>
2017-08-25 00:07:45 +0000

    added to hangman

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,5 +1,6 @@
 require 'byebug'
 class Hangman
+  TOTAL_GUESSES = 8
   attr_reader :guesser, :referee, :board
 
   def initialize(players = {})
@@ -11,21 +12,38 @@ class Hangman
   def setup
     secret_word_length = referee.pick_secret_word
     guesser.register_secret_length(secret_word_length)
-    @board = "_" * secret_word_length
+    @board = [nil] * secret_word_length
   end
 
   def take_turn
-    guessed_letter = guesser.guess
+    guessed_letter = guesser.guess(board)
     correct_letters_indices = referee.check_guess(guessed_letter)
     update_board(guessed_letter, correct_letters_indices)
-    guesser.handle_response
+    guesser.handle_response(guessed_letter, correct_letters_indices)
   end
 
   def update_board(guessed_letter, correct_letters_indices)
     correct_letters_indices.each do |idx|
-      board.each_char.with_index do |space, i|
+      board.each_with_index do |space, i|
         board[i] = guessed_letter if idx == i
       end
+    end
+  end
+
+  def play
+    remaining_guesses = TOTAL_GUESSES
+
+    setup
+    until remaining_guesses == 0 || ((board.include? nil) == false)
+      take_turn
+      update_board(guessed_letter, correct_letters_indices)
+      remaining_guesses -= 1
+    end
+
+    if remaining_guesses != 0
+      puts "You win!"
+    else
+      puts "Maybe next time!"
     end
   end
 end
@@ -39,21 +57,31 @@ class HumanPlayer
   end
 
   def pick_secret_word
+    puts "Enter length of secret word:"
+    secret_word_length = gets.chomp.strip.to_i
   end
 
   def register_secret_length(secret_word_length)
-    "Length of secret word is #{secret_word_length}."
+    puts "Length of secret word is #{secret_word_length}."
   end
 
-  def guess
+  def guess(board)
     puts "Enter your guess:"
     @guess = gets.chomp.strip
   end
 
-  def check_guess
+  def check_guess(guessed_letter)
+    puts "Enter positions where letter \'#{guessed_letter}\' is correct. Example format is \'0, 1\'. Enter \'none\' if \'#{guessed_letter}\' does not appear in your word."
+    answer = gets.chomp.strip
+    if answer == "none"
+      []
+    else
+      answer.split(", ")
+    end
   end
 
-  def handle_response
+  def handle_response(guessed_letter, correct_letters_indices)
+    puts "Keep guessing!"
   end
 end
 
