77c955bb732981de261b7a0ed7d5583425eab71f
Tracie <traciechang@hotmail.com>
2017-09-17 19:12:37 +0000

    change

--- tictactoe-ai/tic_tac_toe_node.rb
+++ tictactoe-ai/tic_tac_toe_node.rb
@@ -1,4 +1,5 @@
 require_relative 'tic_tac_toe'
+require 'byebug'
 
 class TicTacToeNode
   attr_reader :board, :next_mover_mark, :prev_move_pos
@@ -10,47 +11,49 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
-    evaluator.over? && evaluator.winner == :x
+    opp_of_evaluator = (evaluator == :x) ? :o : :x
+    return true if (board.over? && (board.winner == opp_of_evaluator))
+    child_nodes = children
+    child_nodes.each do |child|
+      opp_mark = (child.next_mover_mark == :x) ? :o : :x
+      child.losing_node?(opp_mark)
+    end
+    false
   end
 
   def winning_node?(evaluator)
-    evaluator.over? && evaluator.winner == :o
+    board.over? && board.winner == evaluator
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    child_nodes = []
+    child_mark = switch_next_mover_mark
+    potential_moves = get_potential_moves
+    potential_moves.each do |move|
+      child_nodes << TicTacToeNode.new(create_child_node(move), child_mark, move)
+    end
+    child_nodes
+  end
+  
+  def create_child_node(pos)
+    dup_board = board.dup
+    dup_board[pos] = next_mover_mark
+    dup_board
+  end
+  
+  def get_potential_moves
     potential_moves = []
     (0..2).each do |row|
       (0..2).each do |col|
-        potential_moves << [row, col] if board[[row, col]].empty?
+        potential_moves << [row, col] if board[[row, col]].nil?
       end
     end
     potential_moves
   end
   
-  def create_child_node(pos)
-    dup_board = deep_dup(board)
-    dup_board[pos] = next_mover_mark
-    dup_board
-  end
-  
-  def deep_dup(array)
-    return array unless obj.is_a?(Array)
-    array.map { |el| deep_dup(el) }
-  end
-  
   def switch_next_mover_mark
-    @next_mover_mark = (next_mover_mark == :x) ? :o : :x
+    (next_mover_mark == :x) ? :o : :x
   end
 end
-
-# def some_method
-#   potential_moves = children
-#   potential_moves.each do |move|
-#     create_child_node(move)
-#   end
-# end
-
-create_child_node(move)
-TicTacToeNode.new(create_child_node(move), next_mover_mark) # need to switch next_move_mark somehwere
