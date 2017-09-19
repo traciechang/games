b215696c023e218dcb19358494ee5818f39761f7
Tracie <traciechang@hotmail.com>
2017-09-19 00:16:30 +0000

    add more

--- mancala/board.rb
+++ mancala/board.rb
@@ -25,11 +25,10 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    stones = cups[start_pos].count
     cups[start_pos] = []
     current_pos = start_pos + 1
-    stones = 4
     opp_side = current_player_name == @name1 ? 13 : 6
-    
     until stones == 0
       unless current_pos == opp_side
         cups[current_pos] << :stone 
@@ -44,12 +43,23 @@ class Board
     end
     
     render
-    next_turn(current_pos)
+    if current_pos == 0
+      next_turn(13)
+    else
+      next_turn(current_pos - 1)
+    end
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
-    
+    #debugger
+    # if ending_cup_idx == 6 || ending_cup_idx == 13
+    #   :prompt
+    if cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -61,8 +71,11 @@ class Board
   end
 
   def one_side_empty?
+    ((0..5).all? { |idx| cups[idx].empty? }) || ((7..12).all? { |idx| cups[idx].empty? })
   end
 
   def winner
+    return :draw if cups[6].count == cups[13].count
+    cups[6].count > cups[13].count ? name1 : name2
   end
 end
